3e073f1461d2ec5cd7e8531f3b024d031d1e3ddf
ArunFlashaid <arun@flashaid.in>
2025-08-22 16:40:01 +0530

    port update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { checkConnection } from './db/pgDb.js';
 // http://localhost:3000/api/crudList
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 checkConnection(); 
 
 app.use(cors())
